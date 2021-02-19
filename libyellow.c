bbec5001378bb14e140352d44775d8b9fa6aa5c7
singe <singe-github@singe.za.net>
2021-02-19 12:51:51 +0200

    Specific infrequent alerts is better than noisy alerts

--- libyellow.c
+++ libyellow.c
@@ -14,25 +14,40 @@
       cp libyellow.so /usr/lib
       echo /usr/lib/libyellow.so >> /etc/ld.so.preload
 
+    Edit the list of binary names you want to alert on under "alert_list".
+
   Usage:
     If anyone executes one of your binaries, you'll get a Canary notification.
 **/
 
 #include "canary32.h"
+#define SSIZE_MAX = 64
 
-
-//int main_wrapper(int argc, char *argv[], char *env[])
 int main_wrapper()
 {
+  int alert_size = 2;
+  char *alert_list[] = {
+    "id",
+    "nc"
+  };
+
+  //read name of invoked command from proc
+  //it isn't always being passed from init_array
   FILE *fp;
   fp=fopen("/proc/self/cmdline","r");
   if (fp) {
     char *line = NULL;
     size_t linecap = 0;
-    ssize_t linelen;
     getline(&line, &linecap, fp);
-    yellow(line);
+
+    for (int i=0; i < alert_size; i++) {
+      if(strncmp(line, alert_list[i], 64) == 0 ) {
+        yellow(line);
+        break;
+      }
+    }
   }
+
   return 0;
 }
 
